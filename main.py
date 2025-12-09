81e8be9c3476b8459481ff8bcfc6195cc43328b8
Lucas Guillerot lguiller <lguiller@student.42.fr>
2025-12-09 17:10:31 +0100

    check ticket on billetterie and bourseauxbillets urls
    log title of the concert
    log notified emails
    fix available/unavailable typo

--- main.py
+++ main.py
@@ -12,7 +12,10 @@ import logging
 
 load_dotenv()
 
-URL = "https://billetterie.philharmoniedeparis.fr/list/events"
+URLs = [
+    "https://billetterie.philharmoniedeparis.fr/list/events",
+    "https://bourseauxbillets.philharmoniedeparis.fr/list/events",
+]
 EMAIL_PASSWORD = getenv("EMAIL_PASSWORD")
 TICKET_ID = getenv("TICKET_ID")
 NOTIFY_EMAIL = getenv("NOTIFY_EMAIL")
@@ -63,34 +66,38 @@ def main():
     else:
         driver = webdriver.Firefox(options=options)
     logger.info("Firefox Webdriver initialized")
-    logger.info("Loading page ...")
-    driver.get(URL)
-    logger.info("Page loaded")
-    try:
-        logger.info("Waiting for element to be present ...")
-        element = EC.presence_of_element_located(
-            (By.ID, f"prod_{ticket_id}")
-        )
-        WebDriverWait(driver, WAIT_ELEMENT_TIMEOUT).until(element)
-        logger.info("Element found")
-    except TimeoutException:
-        logger.error("Loading took too much time!")
+    is_available = False
+    for url in URLs:
+        logger.info("Loading page {}".format(url))
+        driver.get(url)
+        logger.info("Page loaded")
+        try:
+            logger.info("Waiting for element to be present ...")
+            element = EC.presence_of_element_located(
+                (By.ID, f"prod_{ticket_id}")
+            )
+            WebDriverWait(driver, WAIT_ELEMENT_TIMEOUT).until(element)
+            logger.info("Element found")
+        except TimeoutException:
+            logger.error("Loading took too much time!")
 
-    logger.info("Parsing page source ...")
-    soup = BeautifulSoup(driver.page_source, "html.parser")
-    logger.info("Page source parsed")
-    logger.info("Checking for ticket availability ...")
-    element = soup.find(id=f"prod_{ticket_id}")
-    is_available = element.find(id=f"product_{ticket_id}_book") is not None
-    logger.info(f"Ticket status: {'Available' if not is_available else 'Unavailable'}")
-    if is_available:
-        logger.info("Sending notification email ...")
-        send_email(
-            subject="Tickets Available!",
-            body=f"Tickets are now available at https://billetterie.philharmoniedeparis.fr/selection/event/date?productId={ticket_id}",
-            to_email=NOTIFY_EMAIL,
-        )
-        logger.info("Notification email sent")
+        logger.info("Parsing page source ...")
+        soup = BeautifulSoup(driver.page_source, "html.parser")
+        logger.info("Checking for concert with id {}".format(ticket_id))
+        element = soup.find(id=f"prod_{ticket_id}")
+        title = element.find(class_="title").get_text(strip=True)
+        logger.info("Concert found: {}".format(title))
+        is_available = element.find(id=f"product_{ticket_id}_book") is not None
+        logger.info(f"Ticket status: {'Available' if is_available else 'Unavailable'}")
+        if is_available:
+            logger.info("Sending notification email to: {}".format(NOTIFY_EMAIL))
+            send_email(
+                subject="Tickets Available!",
+                body=f"Tickets are now available at https://billetterie.philharmoniedeparis.fr/selection/event/date?productId={ticket_id}",
+                to_email=NOTIFY_EMAIL,
+            )
+            logger.info("Notification email sent")
+            break
 
 
 if __name__ == "__main__":
